2f88e5ae232c83b0d0508312a2d72730f2a89ad2
Sazidul Alam <sazidulalam20@gmail.com>
2024-04-24 19:21:44 +0600

    header done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,142 @@
+import PropTypes from "prop-types";
+import { useEffect, useRef, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SlHandbag } from "react-icons/sl";
+import { IoIosSearch } from "react-icons/io";
+
+const SingleNav = ({ pageTitle, path, setIsMobileMenuOpen }) => {
+    return (
+        <NavLink
+            className={({ isActive }) =>
+                isActive
+                    ? "font-medium lg:font-semibold text-lg text-[#FF3811] dark:text-[#FF3811]"
+                    : "font-medium lg:font-semibold text-lg text-[#444444] dark:text-gray-200"
+            }
+            to={path}
+            onClick={() => setIsMobileMenuOpen(false)}
+        >
+            {pageTitle}
+        </NavLink>
+    );
+};
+
+const Header = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const dropdownRef = useRef();
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target)
+            ) {
+                setIsMobileMenuOpen(false);
+            }
+        };
+        if (isMobileMenuOpen) {
+            document.addEventListener("click", handleClickOutside);
+        }
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, [isMobileMenuOpen]);
+
+    const navLinks = (
+        <>
+            <SingleNav
+                setIsMobileMenuOpen={setIsMobileMenuOpen}
+                pageTitle="Home"
+                path="/"
+            />
+            <SingleNav
+                setIsMobileMenuOpen={setIsMobileMenuOpen}
+                pageTitle="About"
+                path="/about"
+            />
+            <SingleNav
+                setIsMobileMenuOpen={setIsMobileMenuOpen}
+                pageTitle="Services"
+                path="/services"
+            />
+            <SingleNav
+                setIsMobileMenuOpen={setIsMobileMenuOpen}
+                pageTitle="Blog"
+                path="/blog"
+            />
+            <SingleNav
+                setIsMobileMenuOpen={setIsMobileMenuOpen}
+                pageTitle="Contact"
+                path="/contact"
+            />
+        </>
+    );
+
+    return (
+        <div className="container mx-auto px-3 md:px-6 py-2">
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <Link>
+                        <img className="w-24" src="/logo.svg" alt="logo" />
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <nav className="flex gap-7">{navLinks}</nav>
+                </div>
+                <div className="navbar-end">
+                    <div className="flex gap-5 mr-6">
+                        <button>
+                            <SlHandbag size={20} />
+                        </button>
+                        <button>
+                            <IoIosSearch size={20} />
+                        </button>
+                    </div>
+                    <button className="btn bg-transparent border border-[#FF3811] text-[#FF3811] hover:bg-[#FF3811] hover:text-white transition-all px-5">
+                        Appointment
+                    </button>
+                    <div className="dropdown" ref={dropdownRef}>
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            className="btn btn-ghost lg:hidden"
+                            onClick={() =>
+                                setIsMobileMenuOpen(!isMobileMenuOpen)
+                            }
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />
+                            </svg>
+                        </div>
+                        {isMobileMenuOpen && (
+                            <nav
+                                tabIndex={0}
+                                className="flex flex-col absolute right-0 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                            >
+                                {navLinks}
+                            </nav>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+SingleNav.propTypes = {
+    pageTitle: PropTypes.string.isRequired,
+    path: PropTypes.string.isRequired,
+    setIsMobileMenuOpen: PropTypes.func.isRequired,
+};
+
+export default Header;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header/Header";
 
 const MainLayout = () => {
     return (
         <div className="font-inter">
+            <Header />
             <Outlet />
         </div>
     );
